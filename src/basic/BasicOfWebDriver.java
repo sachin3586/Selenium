2e06a6d69710617be7c0e81615ffa1403654a222
sachin3586 <sachindadhe55@gmail.com>
2024-09-23 20:58:49 +0530

    Update BasicOfWebDriver.java

--- src/basic/BasicOfWebDriver.java
+++ src/basic/BasicOfWebDriver.java
@@ -7,6 +7,13 @@ public class BasicOfWebDriver {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\lenovo\\Desktop\\Testing\\Driver\\ChromeDriver\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();   // upcasting of chromedriver class
+        
+        /* Instead of Setting property and path we can directly use webdriver manager dependency
+          Setup ChromeDriver using WebDriverManager
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        */
 
         ChromeDriver driver1=new ChromeDriver();  // Child class object
         driver.get("https://gemini.google.com");
